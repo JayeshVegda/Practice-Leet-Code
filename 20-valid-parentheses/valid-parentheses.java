d5cf34afb42b8ef57ac23656803eca4a2c1365c9
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2024-04-16 19:38:20 +0530

    Time: 1 ms (98.59%) | Memory: 40.6 MB (99.87%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,43 @@
+
+class Solution {
+    public static boolean isValid(String s) 
+    {   
+        String expr = s;
+       Deque<Character> stack = new ArrayDeque<Character>();
+
+		// Traversing the Expression
+		for (int i = 0; i < expr.length(); i++) {
+			char x = expr.charAt(i);
+
+			if (x == '(' || x == '[' || x == '{') {
+				stack.push(x);
+				continue;
+			}
+			if (stack.isEmpty())
+				return false;
+			char check;
+			switch (x) 
+			{
+			case ')':
+				check = stack.pop();
+				if (check == '{' || check == '[')
+					return false;
+				break;
+
+			case '}':
+				check = stack.pop();
+				if (check == '(' || check == '[')
+					return false;
+				break;
+
+			case ']':
+				check = stack.pop();
+				if (check == '(' || check == '{')
+					return false;
+				break;
+			}
+		}
+
+		// Check Empty Stack
+		return (stack.isEmpty());
+}}
