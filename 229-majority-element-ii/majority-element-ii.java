70e5d58f8cf33cdd60ef8c0eb5cdbf62fa20e24a
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2024-12-30 09:46:23 +0530

    Time: 2 ms (99.36%) | Memory: 47.8 MB (62.66%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -7,31 +7,38 @@ class Solution {
         int cad1 = Integer.MIN_VALUE;
         int cad2 = Integer.MIN_VALUE;
 
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(vot1 == 0 && nums[i] != cad2 ){
+        for (int i = 0; i < nums.length; i++) {
+            if (vot1 == 0 && nums[i] != cad2) {
                 cad1 = nums[i];
                 vot1 = 1;
-            }else if (vot2 == 0 && nums[i] != cad1){
+            } else if (vot2 == 0 && nums[i] != cad1) {
                 cad2 = nums[i];
                 vot2 = 1;
-            }else if(nums[i] == cad1) {vot1++;}
-            else if(nums[i] == cad2) {vot2++;}
-            else{vot1--; vot2--;}
+            } else if (nums[i] == cad1) {
+                vot1++;
+            } else if (nums[i] == cad2) {
+                vot2++;
+            } else {
+                vot1--;
+                vot2--;
+            }
         }
 
         vot1 = 0;
         vot2 = 0;
 
-        for(int i = 0; i < nums.length; i++){
-                if(nums[i] == cad1) vot1++;
-                if(nums[i] == cad2) vot2++;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == cad1)
+                vot1++;
+            if (nums[i] == cad2)
+                vot2++;
         }
 
         int mini = (int) nums.length / 3 + 1;
-        if(vot1 >= mini) mylist.add(cad1);
-        if(vot2 >= mini) mylist.add(cad2);
-
+        if (vot1 >= mini)
+            mylist.add(cad1);
+        if (vot2 >= mini)
+            mylist.add(cad2);
 
         return mylist;
     }
