914ea019f0ee592d4c6600d1abb038f57219640a
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2025-01-27 12:10:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (87.26%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double temp = x;
+        if(n == 0) return 1;
+        double ans = myPow(x, n/2);
+
+        if(n % 2 == 0){
+            return ans * ans;
+        }else{
+            if(n > 0){
+                return ans * ans * x;
+            }else{
+                return (ans * ans) / x;
+            }
+        }
+    }
+}
