3e8631f1c455172f5e6dc812a01131e26fb699b7
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2025-02-20 14:44:49 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (7.25%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -10,18 +10,20 @@
  * }
  */
 public class Solution {
-    public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while(fast != null && fast.next != null){
-            slow = slow.next;
-            fast = fast.next.next;
+    public boolean hasCycle(ListNode head) 
+    {
+     ListNode slow = head;
+     ListNode fast = head;
+     while(fast != null && fast.next != null)
+     {
+        slow = slow.next;
+        fast = fast.next.next;
 
-            if(slow == fast){
-                return true;
-            }
+        if(fast == slow){
+            return true;
         }
+     }   
 
-        return false;
+     return false;
     }
 }
