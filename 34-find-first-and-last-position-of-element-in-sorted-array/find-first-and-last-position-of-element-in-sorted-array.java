76fc84d8ebe5992d48c53f38747a70d431c019d2
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2025-02-22 20:41:23 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (8.77%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,32 +1,54 @@
 class Solution {
+    public int firstOccurance(int[] nums, int x) {
+       int ans = -1;
+       int left = 0;
+       int right = nums.length - 1;
+
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == x)
+            {
+                ans = mid;
+                right = mid - 1;
+            }else if(nums[mid] < x)
+            {
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    public int SecondOccurance(int[] nums, int x){
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = -1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+
+            if(nums[mid] == x)
+            {
+                ans = mid;
+                left = mid + 1;
+            }else if(nums[mid] < x){
+                left = mid + 1;
+            }
+            else{
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
     public int[] searchRange(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
-        int[] res = {-1, -1};
-        int counter = 0;
+        int first = firstOccurance(nums, target);
+        int second = SecondOccurance(nums, target);
 
+        
 
-        while(start <= end)
-        {
-            if(nums[start] == target){
-                res[0] = start;
-                break;
-            }
-
-            start++;
-        }
-
-        while(start <= end){
-            if(nums[end] == target){
-                res[1] = end;
-                break;
-            }
-
-            end--;
-        }
-
-
-
-        return res;
+        return new int[]{first, second};
     }
 }
