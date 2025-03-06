862989d1a6b131b8613e0b0ebd146dc049b891c7
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2025-03-06 11:22:34 +0530

    Time: 1 ms (93.51%) | Memory: 45.7 MB (34.57%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.java
+++ 724-find-pivot-index/find-pivot-index.java
@@ -1,38 +1,24 @@
 class Solution {
     public int pivotIndex(int[] nums) 
     {
-        HashMap<Integer, Integer> left = new HashMap<>();
-        HashMap<Integer, Integer> right = new HashMap<>();
-
+        int n = nums.length;
+        int totalsum = 0;
         int lsum = 0;
 
-        for(int i = 0; i < nums.length; i++){
-            int val = nums[i];
-            lsum = lsum + val;
-            left.put(i, lsum);
+
+        for(int num : nums){
+            totalsum += num;
         }
 
-        int rsum = 0;
-        for(int i = nums.length - 1; i >= 0; i--)
+        for(int i = 0; i < nums.length; i++)
         {
-            int val = nums[i];
-            rsum = rsum + val;
-            right.put(i, rsum);
-        }
-        
-        System.out.println(left);
-        System.out.println(right);
+            int right = totalsum - lsum - nums[i];
+            if(right == lsum) return i;
 
-        for (int i = 0; i < nums.length; i++) 
-        {
-            int leftTotal = i > 0 ? left.get(i - 1) : 0;
-            int rightTotal = i < nums.length - 1 ? right.get(i + 1) : 0;
+            lsum = lsum + nums[i];
+
             
-            if (leftTotal == rightTotal) {
-                return i;
-            }
         }
-
         return -1;
         
     }
