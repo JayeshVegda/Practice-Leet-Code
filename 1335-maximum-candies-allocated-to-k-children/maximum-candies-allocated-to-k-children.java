687706d8df876c759ff43f1b241fb737ff1116e7
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2025-03-14 10:21:26 +0530

    Time: 42 ms (17.61%) | Memory: 62.9 MB (80.11%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -10,37 +10,19 @@ class Solution {
         }
         return false;
     }
-    public long findMin(int[] arr){
-        long min = 0;
-        for(int num : arr){
-            min = Math.min(num, min);
-        }
-
-        return min;
-    }
-
-    public long findMax(int[] arr){
-        long max = 0;
-        for(int num : arr){
-            max = Math.max(num, max);
-        }
-
-        return max;
-    }
-
-    public long totalSum(int[] arr){
-        long sum = 0;
-        for(int num : arr){
-            sum += num;
-        }
-
-        return sum;
-    }
-
     public int maximumCandies(int[] candies, long k) {
-        if(totalSum(candies) < k) return 0;
-        long left = findMin(candies) + 1;
-        long right = findMax(candies);
+        long sum = 0;
+        int max = 0;
+        
+        for (int candy : candies) {
+            sum += candy;
+            max = Math.max(max, candy);
+        }
+        
+        if (sum < k) return 0;
+
+        long left = 1;
+        long right = max;
         int ans = -1;
 
         while(left <= right){
