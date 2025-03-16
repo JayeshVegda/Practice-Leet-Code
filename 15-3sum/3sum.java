63c069665fa8131ebcaf77c0a7085db4378858bb
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2025-03-16 16:03:09 +0530

    Time: 32 ms (44.72%) | Memory: 52.8 MB (16.16%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,57 +1,59 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> result = new HashSet<>();
-        int n = nums.length;
-        Arrays.sort(nums);
-        for(int i = 0; i < n-2; i++){
-            int left = i + 1;
-            int right = n - 1;
+    public List<List<Integer>> threeSum(int[] arr) {
+        Arrays.sort(arr);
+        int i = 0;
+        int n = arr.length;
+        List<List<Integer>> res = new ArrayList<>();
 
-            while (right > left){
-                int sum = nums[i] + nums[right] + nums[left];
-
-                if(sum == 0){
-                    List<Integer> triplet = new ArrayList<>();
-                    triplet.add(nums[i]);
-                    triplet.add(nums[left]);
-                    triplet.add(nums[right]);
-                    result.add(triplet);
-                    right--;
-                    left++;
+        while(i < n-2)
+        {
+            // check prev
+          
+                if (i > 0 && arr[i] == arr[i - 1]) 
+                {
+                    i++;
+                    continue;
                 }
 
-                else if(sum > 0) {
+            // set left and right pointer
+            int left = i + 1;
+            int right = n - 1;
+            while(left < right)
+            {
+                int a = arr[i];
+                int b = arr[left];
+                int c= arr[right];
+                int sum = a+b+c;
+                
+                if(sum == 0)
+                {
+                    ArrayList<Integer> list = new ArrayList<>();
+                    list.add(a);
+                    list.add(b);
+                    list.add(c);
+                    res.add(list);
+                    // skip duplicate for second element;
+                    while (left < right && arr[left] == arr[left + 1]) {
+                        left++;
+                    }
+                    
+                    // Skip duplicates for third element
+                    while (left < right && arr[right] == arr[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+                }
+                else if(sum > 0){
                     right--;
                 }else{
                     left++;
                 }
-
             }
+            i++;
         }
 
-
-        // int n = nums.length;
-        // HashMap<Integer, Integer> mpp = new HashMap<Integer, Integer>();
-
-        // for(int i = 0; i < n; i++){
-        //     mpp.put(nums[i], i);
-        // }
-
-        // for(int i = 0; i < nums.length; i++){
-            
-        //     for(int j = i; j < nums.length; j++){
-        //         List<Integer> list1 = new ArrayList<>();
-
-        //         int diff = -(nums[j] - nums[i]); //0 
-        //         if(mpp.containsKey(diff)){
-        //             list1.add(nums[i]);
-        //             list1.add(nums[j]);
-        //             list1.add(diff);
-        //             nestedList.add(list1);
-        //         }
-
-        //     }
-        // }
-        return new ArrayList<>(result);
+        return res;
     }
 }
