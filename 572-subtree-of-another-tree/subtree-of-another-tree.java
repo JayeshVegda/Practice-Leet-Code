eb958dc207b22d7f40de70308c22a05aa360dac9
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2025-03-26 12:26:50 +0530

    Time: 2 ms (97.26%) | Memory: 44.8 MB (21.88%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -1,52 +1,36 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
-class Solution {
+class Solution 
+{   
+    public boolean isSame(TreeNode root, TreeNode sub){
+        if(root == null && sub == null) return true;
+        if(root == null || sub == null || root.val != sub.val) return false;
 
-    public boolean isId(TreeNode node, TreeNode subRoot){
-
-        if(node == null && subRoot == null){
-            return true;
-        }else if(node == null || subRoot == null || subRoot.val != node.val){
-            return false;
-        }
-
-        if(!isId(node.left, subRoot.left)){
-            return false;
-        }
-
-        if(!isId(node.right, subRoot.right)){
-            return false;
-        }
-
-        return true;
+        return isSame(root.left, sub.left) && isSame(root.right, sub.right);
     }
-    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root == null)
-        {
-            return false;
-        }
 
-        if(root.val == subRoot.val)
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) 
+    { 
+        if(root == null) return false;
+        if(root.val == subRoot.val) 
         {
-            if(isId(root, subRoot))
-            {
-                return true;
-            }
+            if(isSame(root, subRoot)) return true;
         }
 
         return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+                
     }
 }
