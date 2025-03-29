9b8e4fe874a235b51bb310754572919604665c18
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2025-03-29 19:45:15 +0530

    Time: 1 ms (24.86%) | Memory: 41.8 MB (23.90%) - LeetSync

---
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) 
+    {
+         Queue<TreeNode> q = new LinkedList<>();
+         q.add(root);
+         int level = 0;
+
+        int xParent = 0;
+        int yParent = 0;
+
+         while(!q.isEmpty())
+         {
+            HashSet<Integer> mpp = new HashSet<>();
+
+            int size = q.size();
+            while(size-->0)
+            {
+                TreeNode rem = q.remove();
+
+                if(rem.left != null) q.add(rem.left);
+                if(rem.right != null) q.add(rem.right);
+
+
+                if(rem.left != null)
+                {
+                    if(rem.left.val == x) xParent = rem.val;
+                    if(rem.left.val == y) yParent = rem.val;
+                }
+
+                if(rem.right != null){
+                    if(rem.right.val == x) xParent = rem.val;
+                    if(rem.right.val == y) yParent = rem.val;
+                }
+
+                mpp.add(rem.val);
+            }
+
+            if(mpp.contains(x) && mpp.contains(y) && xParent != yParent) return true;
+         }
+
+
+
+         return false;
+    }
+}
