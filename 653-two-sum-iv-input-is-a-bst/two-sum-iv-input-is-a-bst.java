fc09f7c52a6a4f373701f5c1db499ad74ebd114a
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2025-04-14 22:41:38 +0530

    Time: 3 ms (64.81%) | Memory: 45.6 MB (12.42%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -1,41 +1,30 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        Queue<TreeNode> q = new LinkedList<>();
-        ArrayList<Integer> arr = new ArrayList<>();
-        q.add(root);
+        Set<Integer> set = new HashSet<>();
+        return helper(root, set, k);
+    }
 
-        while(!q.isEmpty()){
-            int size = q.size();
-            while(size-->0){
-                TreeNode rem = q.poll();
-                if(rem.left != null) q.add(rem.left);
-                if(rem.right != null) q.add(rem.right);
+    private boolean helper(TreeNode root, Set<Integer> set, int target) {
+        if (root == null) return false;
 
-                arr.add(rem.val);
-            }
-        }
+        if (set.contains(root.val))return true;
+        set.add(target - root.val);
 
-        for(int i = 0; i < arr.size(); i++){
-            for(int j = i + 1; j < arr.size(); j++){
-                if(arr.get(i) + arr.get(j) == k) return true;
-            }
-        }
-
-        return false;
+        return helper(root.left, set, target) || helper(root.right, set, target);
     }
 }
