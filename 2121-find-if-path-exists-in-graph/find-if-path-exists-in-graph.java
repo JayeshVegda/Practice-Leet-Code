b434b0fba56ab64918e330c1d110515388127706
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2025-05-02 18:22:28 +0530

    Time: 157 ms (41.68%) | Memory: 171.3 MB (47.27%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,32 +1,30 @@
 class Graph{
     int n;
-    ArrayList<Integer>[] adj;
-
-    Graph(int n){
+    List<List<Integer>> adj;
+    Graph(int n)
+    {
         this.n = n;
-        adj = new ArrayList[n];
-
-         for(int i = 0; i < n; i++)
-            adj[i] = new ArrayList<Integer>();
-  
+        adj = new ArrayList<>();
+        for(int i = 0; i < n; i++)
+            adj.add(new ArrayList<Integer>());
     }
 
    
 
     public void adder(int src, int des){
-        adj[src].add(des);
-        adj[des].add(src);
+        adj.get(src).add(des);
+        adj.get(des).add(src);
     }
 
-    public boolean dfs(int src, int des, boolean[] vis){
-        if(vis[src] == true) return false;
-        if(src == des) return true;
+    public boolean dfs(int s, int d, boolean[] vis)
+    {
+        if(vis[s] == true) return false;
+        if(s == d) return true;
 
-        vis[src] = true;
-        ArrayList<Integer> dummy = adj[src];
-        for(int val : dummy)
-        {
-            if(dfs(val, des, vis) == true) return true;
+        vis[s] = true;
+        List<Integer> dummy = adj.get(s);
+        for(int i : dummy){
+            if(dfs(i, d, vis)) return true;
         }
 
         return false;
@@ -34,12 +32,14 @@ class Graph{
 }
 
 class Solution {
+
+
     public boolean validPath(int n, int[][] edges, int source, int destination) {
         Graph g = new Graph(n);
         for(int i = 0; i < edges.length; i++){
             int src = edges[i][0];
-            int des = edges[i][1];
-            g.adder(src, des);
+            int dec = edges[i][1];
+            g.adder(src, dec);
         }
 
         return g.dfs(source, destination, new boolean[n]);
