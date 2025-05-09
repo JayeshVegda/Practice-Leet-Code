0cb4ab6af2f9aadfe369fb733264aea594465d82
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2025-05-09 17:09:04 +0530

    Time: 9 ms (23.53%) | Memory: 45.9 MB (28.71%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,73 @@
+
+class Graph{
+    int n;
+    List<List<Integer>> adj;
+    List<Integer> topo = new ArrayList<>();
+
+    Graph(int n){
+        this.n = n;
+        adj = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<Integer>());
+        }
+    }
+    
+    public void adder(int src, int dest){
+        adj.get(src).add(dest);
+    }
+    
+
+    public boolean helper()
+    {
+        int[] inDeg = new int[n];
+
+        // step 1 : find indegree
+        for(int i = 0; i < n; i++){
+            for(int neg : adj.get(i)){
+                inDeg[neg]++;
+            }
+        }
+
+        // step 2 : add 0 in queue
+        Queue<Integer> q = new LinkedList<>();
+        Stack<Integer> stk = new Stack<>();
+        for(int i = 0; i < n; i++){
+            if(inDeg[i] == 0) q.add(i);
+        }
+        
+        // step 3 : impiment bfs
+        while(!q.isEmpty())
+        {
+            int rem = q.poll();
+
+            for(int neg : adj.get(rem)){
+                inDeg[neg]--;
+                if(inDeg[neg] == 0) q.add(neg);
+            }
+
+            stk.push(rem);
+        }
+
+        // step 4 :
+        return stk.size() == n;
+    }
+
+
+}
+
+class Solution {
+    public boolean canFinish(int V, int[][] edges) 
+    {
+        Graph g = new Graph(V);
+        
+        for(int i = 0; i < edges.length; i++){
+            int src = edges[i][0];
+            int dest = edges[i][1];
+            
+            g.adder(src, dest);
+        }
+        
+        return g.helper();
+        
+    }
+}
