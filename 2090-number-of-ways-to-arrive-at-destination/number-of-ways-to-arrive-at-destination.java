0b36dd7b16f5380803ad91dc2b9884b0eea6f300
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2025-05-21 19:50:23 +0530

    Time: 13 ms (74.62%) | Memory: 49 MB (89.97%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -1,48 +1,99 @@
-class Solution {
-    public int countPaths(int n, int[][] roads) {
-        List<List<int[]>> graph = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+class GPair{
+    int dest;
+    int w;
+
+    GPair(int dest, int w){
+        this.dest = dest;
+        this.w = w;
+    }
+}
+
+class Pair{
+    long dist;
+    int node;
+
+    Pair(long dist, int node){
+        this.dist = dist;
+        this.node = node;
+    }
+}
+
+class Graph{
+    int n;
+    List<List<GPair>> adj;
+
+    Graph(int n){
+        this.n = n;
+        this.adj = new ArrayList<>();
+
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
         }
+    }
+
+    void adder(int src, int dest, int w){
+        adj.get(src).add(new GPair(dest, w));
+        adj.get(dest).add(new GPair(src, w));
+    }
+
+    int djk(){
+        long[] distArr = new long[n];
+        long[] ways = new long[n];
+        Arrays.fill(distArr, Long.MAX_VALUE);
         
-        for (int[] road : roads) {
-            int u = road[0], v = road[1], time = road[2];
-            graph.get(u).add(new int[]{v, time});
-            graph.get(v).add(new int[]{u, time});
-        }
-
-        long[] dist = new long[n];
-        int[] ways = new int[n];
-        Arrays.fill(dist, Long.MAX_VALUE);
-        dist[0] = 0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Long.compare(a.dist, b.dist));
+        distArr[0] = 0;
         ways[0] = 1;
+        pq.add(new Pair(0, 0));
 
-        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(a -> a[0]));
-        pq.offer(new long[]{0, 0});
+        while(!pq.isEmpty())
+        {
+            Pair rem = pq.poll();
 
-        int MOD = 1_000_000_007;
+            long  remDist = rem.dist; // 0
+            int remNode = rem.node; // 0
 
-        while (!pq.isEmpty()) {
-            long[] curr = pq.poll();
-            long d = curr[0];
-            int node = (int) curr[1];
+            for(GPair neb : adj.get(remNode))
+            {
+                int nebNode = neb.dest; // 1
+                int nebDist = neb.w; // 1 
+                long newDist = nebDist + remDist; // 0 + 1
 
-            if (d > dist[node]) continue;
 
-            for (int[] neighbor : graph.get(node)) {
-                int nextNode = neighbor[0];
-                int time = neighbor[1];
-
-                if (dist[node] + time < dist[nextNode]) {
-                    dist[nextNode] = dist[node] + time;
-                    ways[nextNode] = ways[node];
-                    pq.offer(new long[]{dist[nextNode], nextNode});
-                } else if (dist[node] + time == dist[nextNode]) {
-                    ways[nextNode] = (ways[nextNode] + ways[node]) % MOD;
+                if(newDist == distArr[nebNode])
+                {
+                    ways[nebNode] = (ways[nebNode] + ways[remNode]) % 1_000_000_007;
                 }
+
+                if(newDist < distArr[nebNode]) 
+                {
+                    ways[nebNode] = ways[remNode] % 1_000_000_007;
+                    distArr[nebNode] = newDist;
+
+                    pq.add(new Pair(newDist, nebNode));                    
+                }
+
             }
         }
 
-        return ways[n - 1];
+        return (int) ways[n - 1];
+    }
+}
+
+class Solution {
+    public int countPaths(int n, int[][] roads) {
+        Graph g = new Graph(n);
+        
+        for(int i = 0; i < roads.length; i++){
+            int src = roads[i][0];
+            int dest = roads[i][1];
+            int w = roads[i][2];
+
+            g.adder(src, dest, w);
+        }
+
+
+        return g.djk();
+
     }
 }
