480d95feebb4f685b43bfb4e4807be2672986fca
Jayesh Vegda <130431390+JayeshVegda@users.noreply.github.com>
2025-05-30 18:51:32 +0530

    Time: 14 ms (35.37%) | Memory: 61.2 MB (67.25%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -1,57 +1,88 @@
-class Grahp{    
+class DSU{
+    int[] rank;
+    int[] parent;
+    int comp = 0;
     int n;
-    List<List<Integer>> adj;
-
-    Grahp(int n){
+    DSU(int n){
         this.n = n;
-        adj = new ArrayList<>();
-
+        rank = new int[n];
+        parent = new int[n];
         for(int i = 0; i < n; i++){
-            adj.add(new ArrayList<Integer>());
+            rank[i] = 0;
+            parent[i] = i;
         }
     }
 
-    public void adder(int src, int dest){
-        adj.get(src).add(dest);
-        adj.get(dest).add(src);
+    int findParent(int node){
+        if(node == parent[node]){
+            return node;
+        }
+
+        parent[node] = findParent(parent[node]);
+        return parent[node];
     }
 
-    public void dfs(int src, boolean[] vis){
-        if(vis[src] == true) return ;
+    void adder(int u, int v){
+        int s1 = findParent(u);
+        int s2 = findParent(v);
 
-        vis[src] = true;
+        if(s1 == s2) return;
 
-        for(int neg : adj.get(src)){
-            dfs(neg, vis);
+        if(rank[s1] < rank[s2]){
+            parent[s1] = s2;
+        }else if(rank[s2] < rank[s1]){
+            parent[s2] = s1;
+        }else{
+            parent[s2] = s1;
+            rank[s1]++;
         }
     }
+ 
+    int findComp(){
+        int extra = 0;
+        for(int i = 0; i < n; i++){
+            if(parent[i] == i) extra ++;
+        }
+        return extra;
+    }
+
+    void printer(){
+        System.out.print("Rank : ");
+        for(int i = 0; i < n; i++){
+            System.out.print(rank[i] + " ");
+        }
+        System.out.println();
+        System.out.print("Parent : ");
+        for(int i = 0; i < n; i++){
+            System.out.print(parent[i] + " ");
+        }
+    }
+
+
 }
+
 class Solution {
-    public int makeConnected(int n, int[][] mat) {
-        Grahp g = new Grahp(n);
-        int reqEdge = n - 1;
-        if(reqEdge > mat.length) return -1;
+    public int makeConnected(int n, int[][] mat) 
+    {
+        DSU dsu = new DSU(n);
+        int extra = 0;
 
         for(int i = 0; i < mat.length; i++){
             int src = mat[i][0];
             int dest = mat[i][1];
 
-            g.adder(src, dest);
+            if(dsu.findParent(src) == dsu.findParent(dest)){
+                extra++;
+            }else{
+                dsu.adder(src, dest);
+            }
         }
-
-        // finding commponent
-        boolean[] b = new boolean[n];
-        int comp = 0;
-
-        for(int i = 0; i < b.length; i++)
-        {
-            if(b[i] == true) continue;
-            g.dfs(i, b);
-            comp++;
+        int comp = dsu.findComp();
+        System.out.println(extra +" "+  comp);
+        if(extra >= comp - 1){
+            return comp - 1;
+        }else{
+            return -1;
         }
-
-
-        return comp-1;
-
     }
 }
